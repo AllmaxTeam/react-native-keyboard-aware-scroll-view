55c806a9c15ffe767a67e85a220819dd6d792f7b
Dmitriy Shishkin <dshishkin@allmax.team>
2017-08-24 15:08:15 +0700

    Refactor

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 /* @flow */
 
-import KeyboardAwareMixin from './lib/KeyboardAwareMixin'
 import KeyboardAwareScrollView from './lib/KeyboardAwareScrollView'
-import KeyboardAwareListView from './lib/KeyboardAwareListView'
 
-export {
-  KeyboardAwareMixin,
-  KeyboardAwareScrollView,
-  KeyboardAwareListView
-}
+export { KeyboardAwareScrollView }
